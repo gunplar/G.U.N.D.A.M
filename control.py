5153d154aad865d3e2297807016bb6b16e66ca57
Mai <39376416+gunplar@users.noreply.github.com>
2021-06-24 16:11:07 +0200

    Apply internal current angle
    To help with unlocking servo angle

--- control.py
+++ control.py
@@ -8,22 +8,27 @@ class servoThread (threading.Thread):
    global controlKeyPressed
    def __init__(self, servoID, initAngle, angleUpKey, angleDownKey):#, threadQuitKey):
        super().__init__()
-       self.servoID=servoID
-       self.angleUpKey=angleUpKey
-       self.angleDownKey=angleDownKey
+       self.servoID = servoID
+       self.angleUpKey = angleUpKey
+       self.angleDownKey = angleDownKey
+       self.currentAngle = initAngle
        #self.threadQuitKey=threadQuitKey
        kit.servo[servoID].actuation_range = 180
        kit.servo[servoID].set_pulse_width_range(350, 2250)
        kit.servo[servoID].angle = initAngle
        self.start()
    def run(self):
-      while(1):
-         if controlKeyPressed == self.angleUpKey and kit.servo[self.servoID].angle<180:
-             #print(min(kit.servo[self.servoID].angle+4,180))
-             kit.servo[self.servoID].angle = min(kit.servo[self.servoID].angle+4,180)
-         elif controlKeyPressed == self.angleDownKey and kit.servo[self.servoID].angle>0:
-             #print(max(kit.servo[self.servoID].angle-4,0))
-             kit.servo[self.servoID].angle = max(kit.servo[self.servoID].angle-4,0)
+       while(1):
+         if controlKeyPressed == self.angleUpKey and self.currentAngle<180:
+             self.currentAngle = min(self.currentAngle + 4, 180)
+             kit.servo[self.servoID].angle = self.currentAngle
+             time.sleep(0.05)
+             kit.servo[self.servoID].angle = None
+         elif controlKeyPressed == self.angleDownKey and self.currentAngle>0:
+             self.currentAngle = max(self.currentAngle - 4, 0)
+             kit.servo[self.servoID].angle = self.currentAngle
+             time.sleep(0.05)
+             kit.servo[self.servoID].angle = None
          #elif controlKeyPressed == self.threadQuitKey:
          #    kit.servo[self.servoID].angle = None
          #    print("Thread exit",self.servoID)
@@ -32,19 +37,19 @@ class servoThread (threading.Thread):
 
 if __name__=="__main__":
    settings = termios.tcgetattr(sys.stdin)
-   thread1 = servoThread(0,90,'i','k')#,'p')
-   thread2 = servoThread(1,90,'e','d')#,'q')
+   thread1 = servoThread(0,90,'y','h')#,'p')
+   thread2 = servoThread(1,90,'f','v')#,'q')
+   tty.setcbreak(sys.stdin.fileno())
    try:
        while(1):
            #READING KEY IN MAIN THREAD
-            tty.setcbreak(sys.stdin.fileno())
-            rlist, _, _ = select.select([sys.stdin], [], [], None)
+            rlist, _, _ = select.select([sys.stdin], [], [], 0.05)
             if rlist:
                 controlKeyPressed = sys.stdin.read(1)
             else:
                 controlKeyPressed = ''
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
    except Exception as e:
        print(e)
    finally:
        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
